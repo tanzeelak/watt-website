b29917b220d6eb1f39e337c730e361454d4acd88
tanzeelak <tnkhan8042@gmail.com>
2017-07-07 10:58:21 -0700

    bootstrap nav

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+var hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+    res.render('index');
+});
+
+router.get("/about",function(req,res){
+    res.render('about');
+});
+
+router.get("/contact",function(req,res){
+  res.render('contact');
+});
+
+app.use("/",router);
+
+app.use(express.static("public"));
+
+app.use("*",function(req,res){
+    res.render('404');
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
--- src/articles/spooky-action.md
+++
@@ -1,9 +0,0 @@
----
-title: "Spooky Action"
-date: 2016-10-14
-blurb: "What would Einstein make of quantum entanglement today?"
----
-
-## Spooky Action at a Distance
-
-Some text here.
